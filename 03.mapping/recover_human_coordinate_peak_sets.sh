b9e8b26d99f6106dcf02ca1f52e906a5927a0ea9
hxyv <xingyu3906@gmail.com>
2026-04-13 14:50:57 -0400

    Recover human coordinates

---
+++ 03.mapping/recover_human_coordinate_peak_sets.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+mapping_dir="${repo_root}/03.mapping"
+
+human_peaks=${mapping_dir}/human_adrenal_idr_optimal.mapping_preprocess.bed.gz
+human_specific_mapped=${mapping_dir}/human_adrenal_idr_optimal.HumanToMouse.no_mouse_native_overlap.bed.gz
+human_shared_mapped=${mapping_dir}/human_adrenal_idr_optimal.HumanToMouse.shared_with_mouse_native.bed.gz
+
+human_specific_names=${mapping_dir}/human_specific_peak_names.txt
+human_shared_names=${mapping_dir}/human_shared_peak_names.txt
+
+human_specific_original=${mapping_dir}/human_adrenal_idr_optimal.human_specific.original_human_coordinates.bed
+human_shared_original=${mapping_dir}/human_adrenal_idr_optimal.shared.original_human_coordinates.bed
+summary=${mapping_dir}/human_coordinate_recovered_peak_sets_summary.tsv
+
+gzip -dc ${human_specific_mapped} | cut -f4 | sort -u > ${human_specific_names}
+gzip -dc ${human_shared_mapped} | cut -f4 | sort -u > ${human_shared_names}
+
+gzip -dc ${human_peaks} \
+  | awk 'BEGIN { OFS="\t" } NR==FNR { keep[$1]=1; next } ($4 in keep)' ${human_specific_names} - \
+  > ${human_specific_original}
+
+gzip -dc ${human_peaks} \
+  | awk 'BEGIN { OFS="\t" } NR==FNR { keep[$1]=1; next } ($4 in keep)' ${human_shared_names} - \
+  > ${human_shared_original}
+
+gzip -f ${human_specific_original}
+gzip -f ${human_shared_original}
+
+printf "set\tcount\n" > ${summary}
+printf "human_specific_mapped_names\t" >> ${summary}
+wc -l < ${human_specific_names} >> ${summary}
+printf "human_specific_original_human_coordinates\t" >> ${summary}
+gzip -dc ${human_specific_original}.gz | wc -l >> ${summary}
+printf "human_shared_mapped_names\t" >> ${summary}
+wc -l < ${human_shared_names} >> ${summary}
+printf "human_shared_original_human_coordinates\t" >> ${summary}
+gzip -dc ${human_shared_original}.gz | wc -l >> ${summary}
